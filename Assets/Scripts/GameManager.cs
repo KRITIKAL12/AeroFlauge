8b0ab4cbfcf3efe22bde578207da56a0872da44c
KRITIKAL12 <112314943+KRITIKAL12@users.noreply.github.com>
2023-05-07 16:25:51 +0530

    further update before audio
    check

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -58,7 +58,7 @@ public class GameManager : MonoBehaviour
             if (Input.GetKey(KeyCode.Return))
             {
                 
-                SceneManager.LoadScene("Asteroids");
+                SceneManager.LoadScene("Menu");
                 
             }
         }
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,6 +10,26 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void Level1()
+    {
+        SceneManager.LoadScene("Directional Aesteroids 1");
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene("Level 2 Aesteroids 2");
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene("Level 3 Aesteroids 3");
+    }
+
+    public void Level4()
+    {
+        SceneManager.LoadScene("Level 4 Aesteroids 4");
+    }
+
     public void QuitGame()
     {
         Debug.Log("Quit");
