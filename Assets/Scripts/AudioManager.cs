0c9ec9d0b54d2933d331932004c5b049397dcee2
KRITIKAL12 <112314943+KRITIKAL12@users.noreply.github.com>
2023-05-10 16:51:54 +0530

    Asteroids red to match the rocket

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
 
+    public static AudioManager instance;
     public Sound[] sounds;
 
     // Start is called before the first frame update
     void Awake()
     {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
         DontDestroyOnLoad(gameObject);
         foreach (Sound s in sounds)
         {
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private AudioSource click;
+    //[SerializeField] private AudioSource click;
     /*public void PlayGameMusic()
     {
         FindObjectOfType<AudioManager>().Play("Click");
@@ -14,8 +14,8 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
-        click.Play();
-        StartCoroutine(LoadNextSceneWithDelay(0.1f));
+        //click.Play();
+        StartCoroutine(LoadNextSceneWithDelay(0f));
     }
 
     private IEnumerator LoadNextSceneWithDelay(float delay)
@@ -24,27 +24,27 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public async void Level1()
+    public void Level1()
     {
-        await Task.Delay(100); // Delay for 100 milliseconds (0.1 seconds)
+        //await Task.Delay(100); // Delay for 100 milliseconds (0.1 seconds)
         SceneManager.LoadScene("Directional Aesteroids 1");
     }
 
-    public async void Level2()
+    public void Level2()
     {
-        await Task.Delay(100); // Delay for 100 milliseconds (0.1 seconds)
+        //await Task.Delay(100); // Delay for 100 milliseconds (0.1 seconds)
         SceneManager.LoadScene("Level 2 Aesteroids 2");
     }
 
-    public async void Level3()
+    public void Level3()
     {
-        await Task.Delay(100); // Delay for 100 milliseconds (0.1 seconds)
+        //await Task.Delay(100); // Delay for 100 milliseconds (0.1 seconds)
         SceneManager.LoadScene("Level 3 Aesteroids 3");
     }
 
-    public async void Level4()
+    public void Level4()
     {
-        await Task.Delay(100); // Delay for 100 milliseconds (0.1 seconds)
+        //await Task.Delay(100); // Delay for 100 milliseconds (0.1 seconds)
         SceneManager.LoadScene("Level 4 Aesteroids 4");
     }
 
