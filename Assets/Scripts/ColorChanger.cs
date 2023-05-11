126140cfc789eb0808e87594f5c1015de3d72a11
KRITIKAL12 <112314943+KRITIKAL12@users.noreply.github.com>
2023-05-11 14:27:56 +0530

    Blinking Spaceship

--- Assets/Scripts/ColorChanger.cs
+++ Assets/Scripts/ColorChanger.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class ColorChanger : MonoBehaviour
@@ -6,11 +7,47 @@ public class ColorChanger : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     private bool isCyan = true;
 
+    private bool isBlinking = false;
+
+    public float blinkDuration = 3.0f;
+    public float blinkInterval = 0.2f;
+
+
     void Start()
     {
         // Get the SpriteRenderer component of the object
         spriteRenderer = GetComponent<SpriteRenderer>();
+
+        StartCoroutine(BlinkPlayer());
     }
+
+    private IEnumerator BlinkPlayer()
+    {
+        isBlinking = true;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < blinkDuration)
+        {
+            yield return new WaitForSeconds(blinkInterval);
+
+            // Toggle the player's color between red and cyan
+            if (spriteRenderer.color == Color.red)
+            {
+                spriteRenderer.color = Color.cyan;
+            }
+            else
+            {
+                spriteRenderer.color = Color.red;
+            }
+
+            elapsedTime += blinkInterval;
+        }
+
+        // Reset the color to the original color after blinking stops
+        spriteRenderer.color = Color.red;
+        isBlinking = false;
+    }
+
 
     void Update()
     {
